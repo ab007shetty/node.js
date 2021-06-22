e9f4d598dad97c8bb2b99024a994c6e105a3c887
Anirudha B Shetty <45870174+ab007shetty@users.noreply.github.com>
2021-06-22 12:42:36 +0530

    Simple http server

--- node-http/index.js
+++ node-http/index.js
@@ -1,15 +1,53 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = "localhost";
 const port= 3000;
 
 const server = http.createServer((  req,res) => {
 
-    console.log(req.headers);
+    console.log("Request for" + req.url + ' By method ' + req.method);
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1> Hello World !</h1></body></html>')
+   if (req.method == 'GET') {
+        var fileUrl;
+        if (req.url == '/') fileUrl = '/index.html';
+        else fileUrl = req.url;
+
+        var filePath = path.resolve('./public'+fileUrl);
+        const fileExt = path.extname(filePath);
+
+        if(fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if (!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html><body><h1> Error 404: '+ fileUrl + ' not Found. </h1></body></html>');
+
+                    return;
+                }
+
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            })
+        }
+        else{
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1> Error 404: '+ fileUrl + ' not an HTML file. </h1></body></html>');
+
+            return;
+        }
+   }
+   else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1> Error 404: '+ req.meth + ' not Supported. </h1></body></html>');
+
+        return;
+
+   }
 })
 
 server.listen(port, hostname, () => {
