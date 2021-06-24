690a0131f1b3c86c80c0d26075beb3ebf82cbcb8
Anirudha B Shetty <45870174+ab007shetty@users.noreply.github.com>
2021-06-24 22:00:28 +0530

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,17 +8,14 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
   console.log("Connected correctly to server");
 
-  var newDish = Dishes({
+  Dishes.create({
     name: "Uthappizza",
     description: "test",
-  });
-
-  newDish
-    .save()
+  })
     .then((dish) => {
       console.log(dish);
 
-      return Dishes.find({});
+      return Dishes.find({}).exec();
     })
     .then((dishes) => {
       console.log(dishes);
