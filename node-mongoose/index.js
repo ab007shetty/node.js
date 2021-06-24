d6eb1a42f559e04826799200513df1820aa4fa79
Anirudha B Shetty <45870174+ab007shetty@users.noreply.github.com>
2021-06-24 22:25:12 +0530

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -15,10 +15,29 @@ connect.then((db) => {
     .then((dish) => {
       console.log(dish);
 
-      return Dishes.find({}).exec();
+      return Dishes.findByIdAndUpdate(
+        dish._id,
+        {
+          $set: { description: "Updated test" },
+        },
+        {
+          new: true,
+        }
+      ).exec();
     })
-    .then((dishes) => {
-      console.log(dishes);
+    .then((dish) => {
+      console.log(dish);
+
+      dish.comments.push({
+        rating: 5,
+        comment: "I'm getting a sinking feeling!",
+        author: "Leonardo di Carpaccio",
+      });
+
+      return dish.save();
+    })
+    .then((dish) => {
+      console.log(dish);
 
       return Dishes.remove({});
     })
--- node-mongoose/models/dishes.js
+++ node-mongoose/models/dishes.js
@@ -1,6 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
 const dishSchema = new Schema(
   {
     name: {
@@ -12,6 +34,7 @@ const dishSchema = new Schema(
       type: String,
       required: true,
     },
+    comments: [commentSchema],
   },
   {
     timestamps: true,
