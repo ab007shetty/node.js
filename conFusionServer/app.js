b9ba88836e18da12bfbfa11d87d5a1eeb7c80e2a
Anirudha B Shetty <45870174+ab007shetty@users.noreply.github.com>
2021-06-28 01:09:35 +0530

    Express Sessions Part 1

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -19,12 +21,20 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-6789-0604-4868'));
+// app.use(cookieParser('12345-6789-0604-4868'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-6789-0604-4868',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore
+}));
 
 function auth(req, res, next) {
-  console.log(req.signedCookies);
+  console.log(req.session);
 
-  if(!req.signedCookies.user) {
+  if(!req.session.user) {
 
     var authHeader = req.headers.authorization;
 
@@ -41,7 +51,7 @@ function auth(req, res, next) {
     var password = auth[1];
   
     if(username === 'admin' && password === 'password') {
-      res.cookie('user', 'admin', { signed: true})
+      req.session.user = 'admin';
       next();
     }
     else {
@@ -52,7 +62,7 @@ function auth(req, res, next) {
     }
   }
   else {
-    if(req.signedCookies.user === 'admin') {
+    if(req.session.user === 'admin') {
       next();
     }
     else {
