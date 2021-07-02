f8ad05884178d7cb73086e52f66e69df85e518be
Anirudha B Shetty <45870174+ab007shetty@users.noreply.github.com>
2021-07-02 15:55:41 +0530

    Express sessions part 2

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -31,43 +31,24 @@ app.use(session({
   store: new FileStore
 }));
 
-function auth(req, res, next) {
-  console.log(req.session);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+function auth (req, res, next) {
+    console.log(req.session);
 
   if(!req.session.user) {
-
-    var authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      var err = new Error('Yor are Not Authenticated!');
-
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
       return next(err);
-    }
-  
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');  
-    var username = auth[0];
-    var password = auth[1];
-  
-    if(username === 'admin' && password === 'password') {
-      req.session.user = 'admin';
-      next();
-    }
-    else {
-      var err = new Error('Yor are Not Authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
   }
   else {
-    if(req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
       next();
     }
     else {
-      var err = new Error('Yor are Not Authenticated!');
-      err.status = 401;
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
       return next(err);
     }
   }
@@ -78,8 +59,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
