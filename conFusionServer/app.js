5a85fad1a8cfa723a1d612956270e43d8999d61c
Anirudha B Shetty <45870174+ab007shetty@users.noreply.github.com>
2021-07-05 17:54:08 +0530

    HTTPS

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -15,7 +15,30 @@ var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
 
+const mongoose = require("mongoose");
+const url = config.mongoUrl;
+const connect = mongoose.connect(url);
+
+connect.then(
+  (db) => {
+    console.log("Connected correctly to the server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
 var app = express();
+
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -36,19 +59,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
-
-const mongoose = require("mongoose");
-const url = config.mongoUrl;
-const connect = mongoose.connect(url);
-
-connect.then(
-  (db) => {
-    console.log("Connected correctly to the server");
-  },
-  (err) => {
-    console.log(err);
-  }
-);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
