4ae1990b481770d63605cc62b262151af4cea9c9
Anirudha B Shetty <45870174+ab007shetty@users.noreply.github.com>
2021-07-06 01:21:19 +0530

    Passport Facebook

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -4,6 +4,7 @@ var User = require('./models/user');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var config = require('./config');
 
@@ -36,18 +37,41 @@ exports.jwtPasssport = passport.use(new JwtStrategy(opts, (jwt_payload, done) =>
 
 exports.verifyUser = passport.authenticate('jwt', {session : false});
 
-exports.verifyAdmin = function(req, res, next) {
-    User.findOne({_id: req.user._id})
-    .then((user) => {
-        console.log("User: ", req.user);
-        if (user.admin) {
+exports.verifyAdmin = (req, res, next) => {
+        if (req.user.admin) {
             next();
         }
         else {
-            err = new Error('You are not authorized to perform this operation!');
+            var err = new Error('You are not authorized to perform this operation!');
             err.status = 403;
             return next(err);
         } 
-    }, (err) => next(err))
-    .catch((err) => next(err))
-}
+    };
+
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+        clientID: config.facebook.clientId,
+        clientSecret: config.facebook.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({facebookId: profile.id}, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            if (!err && user !== null) {
+                return done(null, user);
+            }
+            else {
+                user = new User({ username: profile.displayName });
+                user.facebookId = profile.id;
+                user.firstname = profile.name.givenName;
+                user.lastname = profile.name.familyName;
+                user.save((err, user) => {
+                    if (err)
+                        return done(err, false);
+                    else
+                        return done(null, user);
+                })
+            }
+        });
+    }
+));
--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '345073950541828',
+        clientSecret: 'ebd3d2109bcd2a532d51fcb0b178db60'
+    }
 }
