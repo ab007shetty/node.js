416d806fd381ec6fa6db6adff4103cd61782811c
Anirudha B Shetty <45870174+ab007shetty@users.noreply.github.com>
2021-07-05 18:34:09 +0530

    File Upload

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -14,6 +14,8 @@ var usersRouter = require("./routes/users");
 var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
+var uploadRouter = require('./routes/uploadRouter');
+
 
 const mongoose = require("mongoose");
 const url = config.mongoUrl;
@@ -59,6 +61,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
+app.use('/imageUpload',uploadRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
