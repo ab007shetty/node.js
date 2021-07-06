147f7a298d47fd2de0c5853fdb4289b1e56ebe50
Anirudha B Shetty <45870174+ab007shetty@users.noreply.github.com>
2021-07-07 00:21:13 +0530

    ConFusion React Firebase

---
+++ conFusion-React-Firebase/README.md
@@ -0,0 +1,13 @@
+## How to Setup and Run
+
+1.Install the Dependencies
+
+```sh
+    yarn install --ignore-engines
+```
+
+2.Execution
+
+```sh
+    yarn start
+```
---
+++ conFusion-React-Firebase/src/firebase/config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyCAydiibLiJBoIoXafwYiMyX5mBl5jd0TY",
+  authDomain: "confusionserver-4fec7.firebaseapp.com",
+  databaseURL: "Your Database URL",
+  projectId: "confusionserver-4fec7",
+  storageBucket: "confusionserver-4fec7.appspot.com",
+  messagingSenderId: "68604281795"
+};
