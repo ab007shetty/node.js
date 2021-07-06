2b2d63be5f39430e9ce7a8e266e601e004807d13
Anirudha B Shetty <45870174+ab007shetty@users.noreply.github.com>
2021-07-06 12:24:30 +0530

    Final Assignment

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -15,8 +15,9 @@ var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
 var uploadRouter = require('./routes/uploadRouter');
+var favoriteRouter = require('./routes/favoriteRouter')
 
-
+// Connection URL
 const mongoose = require("mongoose");
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
@@ -62,7 +63,7 @@ app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
 app.use('/imageUpload',uploadRouter);
-
+app.use('/favorites',favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
